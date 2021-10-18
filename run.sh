591655d45ead76b6446b1d6f639e62adfe283c8f
Siddharth Srinivasan <sriniv75@purdue.edu>
2021-10-17 22:24:42 -0400

    Added run commands

--- run.sh
+++ run.sh
@@ -1,3 +1,5 @@
 #!/bin/bash
 python3 src/most_active_cookie.py -l cookie_log.csv -d 2018-12-09
 python3 src/most_active_cookie.py -l cookie_log.csv -d 2018-12-08
+python3 src/most_active_cookie.py -l cookie_log.csv -d 2018-12-07
+python3 src/most_active_cookie.py -l cookie_log.csv -d 2018-12-06
